7cbed152625f4878da07cf6b09f5bb8cc29fc5aa
johnvoyage <jmyork762@gmail.com>
2018-04-07 10:58:56 -0400

    second commit

--- chartschartscharts.js
+++ chartschartscharts.js
@@ -1,6 +1,12 @@
 document.addEventListener('DOMContentLoaded', () => {
-  let testDiv = document.createElement('div')
-  testDiv.innerText = "divtext"
-  document.body.append(testDiv)
+  let submitInfoForm = document.createElement('form')
+  submitInfoForm.id = 'submit-form'
+  submitInfoForm.innerHTML =
+  `
+    <input type="number" placeholder="X axis data">
+    <input type="text" placeholder="Y axis data">
+    <input type="submit">
+  `
+  $('#submit-form-div').append(submitInfoForm)
 
 })
